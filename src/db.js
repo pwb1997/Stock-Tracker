7474fbd62bb8772ba2f67239a02cce479cb8cce5
Puwei Bao <pwb1997@hotmail.com>
2017-11-09 11:30:06 -0500

    update schema

--- src/db.js
+++ src/db.js
@@ -1,20 +1,31 @@
+const timestamp = require('mongoose-timestamp');
+const slug = require('mongoose-slug');
 const mongoose = require('mongoose');
 
-const stockTracker = new mongoose.Schema({
+const Portfolio = new mongoose.Schema({
+    name: String,
+    quantity: Number,
+    original: Number,
+    current: Number
+});
+
+const PortfolioBook = new mongoose.Schema({
     username: String,
     hash: String,
     portfolios: [{
         user: String,
         portfolioName: String,
-        portfolioItems: [{
-            name: String,
-            quantity: Number,
-            original: Number,
-            current: Number
-        }],
-        createdAt: String,
+        portfolioItems: [Portfolio]
     }],
 });
 
-mongoose.model('stockTracker', stockTracker);
+Portfolio.plugin(timestamp);
+Portfolio.plugin(slug);
+
+PortfolioBook.plugin(timestamp);
+PortfolioBook.plugin(slug);
+
+
+mongoose.model('Portfolio', Portfolio);
+mongoose.model('PortfolioBook', PortfolioBook);
 mongoose.connect('mongodb://localhost/stockTracker');
