88f9a35079f6b9dfb215682c590ec53a32d0f5cf
Puwei Bao <pwb1997@hotmail.com>
2017-11-20 15:51:28 -0500

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-~/usr/local/lib/node_modules/.bin/forever -o ~/var/log/app.log -e ~/var/log/app_error.log start ~/opt/pb1713-final-project/src/server.js
+~/usr/local/lib/node_modules/.bin/forever -o ~/var/log/app.log -e ~/var/log/app_error.log start ~/opt/pb1713-final-project/server.js
